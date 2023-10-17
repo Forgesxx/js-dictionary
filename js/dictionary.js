d55c77bd6d6a46991e4f3e0f65430813cba47572
ROG\forge <Forgesx236@gmail.com>
2023-10-17 14:01:13 +0300

    I'm learning a Javascript vocabulary

---
+++ js/dictionary.js
@@ -0,0 +1,26 @@
+
+//let myDicionary = {
+    //password1: '1',
+    //password2: '2',
+   // password3: '3'
+//};
+//for (let key in myDicionary) {
+    //let mean = myDicionary[key];
+    //console.log(`key: ${key} mean: ${mean}`);
+//}
+let userInfo = {
+    Name: "Ilya",
+    Age: 17,
+    adress: {
+        Sity: "Kyiv",
+        Street: "Saperno-Slobidska",
+        House: 22,
+    }
+}
+for (let key in userInfo){
+    console.log(key)
+    console.log(userInfo[key])
+
+
+}
+
